1838e793f86a3488587d7346ee027e2807a53e30
anisbena <bena169@uqo.ca>
2026-04-14 18:58:16 -0400

    Fix CORS pour Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 const app = express();
 
 /* ── Middleware ── */
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://parking-multiniveaux.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
